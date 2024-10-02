ef31443ba31e3bb5a1e9c54846f6355e4e1ecfa9
ZevCe <cellaz@rpi.edu>
2024-10-02 17:12:29 -0400

    Code layout in place

---
+++ app/src/main/java/com/example/screenlife2/Batch.java
@@ -0,0 +1,99 @@
+package com.example.screenlife2;
+
+//import com.screenomics.Constants;
+
+import java.io.File;
+
+//probably an inefficient data type to be using
+import java.util.List;
+
+
+//need to find out if there are plans on how we should upload the files
+/*
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+
+//class for sending a batch of files all at once
+public class Batch {
+
+    private final MediaType PNG = MediaType.parse("image/*");
+
+    //Variable which keeps track of all the images
+    private final List<File> files;
+
+    private final OkHttpClient client;
+
+    //constructor, takes a list of files to send and a client
+    //to send the files to
+    Batch(List<File> files, OkHttpClient client) {
+        this.files = files;
+        this.client = client;
+    }
+
+    //Function to actually send the files
+    public String sendFiles() {
+
+        //MultipartBody Builder is a class for adding data to http packets easily
+        MultipartBody.Builder bodyPart = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM);
+
+        //iterating through all our files and checking their validity
+        //if the file corresponds to an actual image then we add it to the packet
+        for (int i = 0; i < files.size(); i++) {
+            if (files.get(i).isFile()) {
+                bodyPart.addFormDataPart("file" + (i + 1), files.get(i).getName(), RequestBody.create(PNG, files.get(i)));
+            }
+        }
+
+        //Turning our data into an actual packet to be sent to client
+        RequestBody body = bodyPart.build();
+        Request request = new Request.Builder()
+                .addHeader("Content-Type", "multipart/form-data")
+                .url(Constants.UPLOAD_ADDRESS)
+                .post(body)
+                .build();
+
+        //initiating variable to store response from client
+        Response response = null;
+        try {
+            //variable to keep track of how long our upload takes
+            long startTime = System.nanoTime();
+            //sending the upload request and storing the response
+            response = client.newCall(request).execute();
+            //printing out how long upload too to our console
+            System.out.println("Upload of " + files.size() + " files took " + (System.nanoTime() - startTime)/1000000 + "ms");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //turning our response into aa code
+        int code = response != null ? response.code() : 999;
+        //checking the range of the code
+        if (code >= 400  && code < 500) {
+            System.out.println(response.toString());
+        }
+        //closing the connection if we got a response
+        if (response != null) response.close();
+        //returning the response sent by client
+        return String.valueOf(code);
+    }
+
+    //deletes all the files currently stored on users device
+    public void deleteFiles() {
+        files.forEach(File::delete);
+    }
+
+    //Returning the number of files being sent
+    public int size() {
+        return files.size();
+    }
+
+}
+
+ */
---
+++ app/src/main/java/com/example/screenlife2/UploadScheduler.java
@@ -0,0 +1,393 @@
+package com.example.screenlife2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.preference.PreferenceManager;
+
+import java.io.File;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+// The main upload service
+public class UploadScheduler {
+    public enum UploadStatus
+    {
+        IDLE,
+        UPLOADING,
+        FAILED,
+        SUCCESS
+    }
+    private UploadStatus uploadStatus = UploadStatus.IDLE;
+
+    //interval between attempted uploads in minutes
+    //could probably be turned into a const?
+    private final long uploadInterval = 60;
+
+    //For keeping track of if we are connected to wifi
+    //will need to figure out how to update this
+    private boolean wifiConnection = false;
+
+
+    // The scheduler
+    private final ScheduledExecutorService scheduler =
+            Executors.newScheduledThreadPool(1);
+    // The current runnable task of the scheduler
+    private ScheduledFuture<?> uploadHandle = null;
+
+    // Schedules to start
+    public void startUpload() {
+        stopUpload();
+        Runnable uploadRunner = this::uploadImages;
+        uploadHandle = scheduler.scheduleWithFixedDelay(uploadRunner, uploadInterval, uploadInterval, TimeUnit.MINUTES);
+        uploadStatus = UploadStatus.UPLOADING;
+    }
+
+    // Stops uploading of images
+    public void stopUpload(){
+        if (uploadHandle == null)
+            return;
+        //insertPauseImage();
+        uploadHandle.cancel(false);
+        uploadHandle = null;
+        uploadStatus = UploadStatus.IDLE;
+    }
+
+    //still not sure where/how this is getting called
+    //presumably the startForegroundService call will call one of the other files in this folder
+    //and that is where all the intent/context variables originate from
+    public void uploadImages(Context context) {
+        File f_encrypt =
+                new File(context.getExternalFilesDir(null).getAbsolutePath() + File.separator +
+                        "encrypt");
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        String participant = prefs.getString("participant", "MISSING_PARTICIPANT_NAME");
+        String key = prefs.getString("participantKey", "MISSING_KEY");
+
+        //ask about what intents do, preferably we don't need a second class
+        //
+        Intent intent = new Intent(context, UploadService.class);
+        intent.putExtra("dirPath", f_encrypt.getAbsolutePath());
+        intent.putExtra("continueWithoutWifi", wifiConnection);
+        intent.putExtra("participant", participant);
+        intent.putExtra("key", key);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(intent);
+        }
+    }
+
+    /*
+    public String[] getCaptures()
+    {
+        return null;
+    }
+    */
+    public UploadStatus getUploadStatus() { return uploadStatus; }
+}
+
+
+/*
+
+keeping commented out here for reference, in original project
+upload scheduler would call upload service which would then
+break files down into batches and send them, however we are not using the same
+classes/libraries so the same approach does not apply
+
+package com.screenomics.services.upload;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Binder;
+import android.os.Build;
+import android.os.IBinder;
+import android.preference.PreferenceManager;
+
+import com.screenomics.Constants;
+import com.screenomics.R;
+import com.screenomics.util.InternetConnection;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+
+
+public class UploadService extends Service {
+
+    private final OkHttpClient client =
+            new OkHttpClient.Builder().readTimeout(Constants.REQ_TIMEOUT, TimeUnit.SECONDS).build();
+    public int numToUpload = 0;
+    public int numUploaded = 0;
+    public int numTotal = 0;
+    public boolean uploading = false;
+    public Status status = Status.IDLE;
+    public String errorCode = "";
+    public String lastActivityTime = "";
+    public boolean continueWithoutWifi = false;
+
+    private int numBatchesSending = 0;
+    private int numBatchesToSend = 1;
+    private final List<Batch> batches = new ArrayList<>();
+    private final FileFilter onlyFilesBeforeStart = new FileFilter() {
+        @Override
+        public boolean accept(File file) {
+            List<String> parts = Arrays.asList(file.getName().replace(".png", "").split("_"));
+            // May 2nd fix: move 1 back in the list because of new descriptor component
+            Integer[] dP =
+                    parts.subList(parts.size() - 7, parts.size() - 1).stream().map(Integer::valueOf).toArray(Integer[]::new);
+            LocalDateTime imageCreateTime = LocalDateTime.of(dP[0], dP[1], dP[2], dP[3], dP[4],
+                    dP[5]);
+            return imageCreateTime.isBefore(LocalDateTime.now());
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    private void sendNextBatch() {
+        //making sure we have batches to send
+        if (batches.isEmpty()) return;
+
+        //making sure we arent sending more than the maximum allowed number of batches
+        if (numBatchesSending >= numBatchesToSend) return;
+
+        //checking user has wifi access
+        if (!continueWithoutWifi && !InternetConnection.checkWiFiConnection(this))
+            sendFailure("NOWIFI");
+
+        //getting the first batch to send from our list of batches
+        Batch batch = batches.remove(0);
+
+        //incrementing our variable keeping track of how many batches we are sending
+        numBatchesSending++;
+        System.out.println("SENDING NEXT BATCH, numBatchesSending " + numBatchesSending + " out " +
+                "of " + numBatchesToSend + " with " + batch.size() + " images");
+
+        //creates a sender worker to upload the batches?
+        new Sender().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, batch);
+    }
+
+    //if a batch was sent successfully update corresponding variables
+    private void sendSuccessful(Batch batch) {
+        //decrement number of batches we are currently sending
+        numBatchesSending--;
+        //update total number of files sent
+        numUploaded += batch.size();
+        //update the number of files we still need to send
+        numToUpload -= batch.size();
+
+        //checking if the system should allow for more batches to be sent
+        if (numBatchesToSend < Constants.MAX_BATCHES_TO_SEND) numBatchesToSend++;
+
+        //sending more batches, may be able to compare directly to the constant, will change later
+        for (int i = 0; i < numBatchesToSend; i++) {
+            sendNextBatch();
+        }
+
+        //terminating program if we have sent as many files as necessary
+        if (numToUpload <= 0) {
+            System.out.println("Sending Successful!");
+            status = Status.SUCCESS;
+            reset();
+            stopForeground(true);
+            stopSelf();
+        }
+    }
+
+    //for when we fail to send a batch
+    private void sendFailure(String code) {
+        status = Status.FAILED;
+        errorCode = code;
+        setNotification("Failure in Uploading", "Error code: " + errorCode);
+        reset();
+    }
+
+    //resetting all the important counters in the class so we can upload more files
+    private void reset() {
+        ZonedDateTime dateTime = ZonedDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        lastActivityTime = dateTime.format(formatter);
+        uploading = false;
+        numBatchesToSend = 0;
+        numTotal = 0;
+    }
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        //clearing current notifications
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.cancelAll();
+
+        //creating notification to let user know we are uploading
+        createNotificationChannel();
+        Notification notification = setNotification("Uploading..", "Preparing..");
+
+        // Notification ID cannot be 0.
+        startForeground(5, notification);
+
+        //checking if there is no intent to upload or we are already sending
+        if (intent == null || status == Status.SENDING) {
+            stopForeground(true);
+            stopSelf();
+            return super.onStartCommand(intent, flags, startId);
+        }
+
+        //getting path where files are stored as well as whether we should upload without wifi
+        String dirPath = intent.getStringExtra("dirPath");
+        continueWithoutWifi = intent.getBooleanExtra("continueWithoutWifi", false);
+
+        //getting the directory of our files and grabbing all files that were captured before starting upload
+        File dir = new File(dirPath);
+        File[] files = dir.listFiles(onlyFilesBeforeStart);
+
+        //if there are no files then stop the upload
+        if (files == null || files.length == 0) {
+            stopForeground(true);
+            stopSelf();
+            return super.onStartCommand(intent, flags, startId);
+        }
+
+        //creating a list of all our files to upload
+        LinkedList<File> fileList = new LinkedList<>(Arrays.asList(files));
+
+        //getting the number of files per batch and the number of files to send overall
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        int batchSize = prefs.getInt("batchSize", Constants.BATCH_SIZE_DEFAULT);
+        int maxToSend = prefs.getInt("maxSend", Constants.MAX_TO_SEND_DEFAULT);
+
+        //resetting our counter for how many files we are attempting to upload
+        numToUpload = 0;
+
+        System.out.println("INITIAL FILELIST SIZE " + fileList.size() + "");
+
+        //split our list of files into batches
+        while (!fileList.isEmpty() && (maxToSend == 0 || numToUpload < maxToSend)) {
+            //creating a list of files for a single batch
+            List<File> nextBatch = new LinkedList<>();
+
+            //iterating through our list of files adding them to our nextBatch list while
+            //ensuring that we do not send to many in one batch and that the file list does not run out
+            for (int i = 0; i < batchSize && (maxToSend == 0 || numToUpload < maxToSend) && !fileList.isEmpty(); i++) {
+                numToUpload ++;
+                nextBatch.add(fileList.remove());
+            }
+
+            //creating a new batch with our list of files and adding it to our array of batches
+            Batch batch = new Batch(nextBatch, client);
+            batches.add(batch);
+        }
+
+        //setting our counter variables for sending the first batch
+        numTotal = numToUpload;
+        numUploaded = 0;
+        numBatchesToSend = 1;
+        System.out.println("GOT " + batches.size() + " BATCHES WITH " + numToUpload + "IMAGES TO " +
+                "UPLOAD");
+        System.out.println("TOTAL OF " + fileList.size() + " IMAGES THO");
+
+        //updating status so system knows we are sending
+        status = Status.SENDING;
+        // Send the first batch, on success will cause more batches to be sent
+        //on failure will stop the system
+        sendNextBatch();
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return new LocalBinder();
+    }
+
+    //creating the notification to let the user know an upload is happening
+    //lots of syntactic sugar I don't fully understand
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    "uploading-channel",
+                    "Screenomics Service Channel",
+                    NotificationManager.IMPORTANCE_LOW
+            );
+            serviceChannel.setSound(null, null);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(serviceChannel);
+        }
+    }
+
+    //lots more syntactic sugar I don't fully understand, seems to just be a way
+    //to set a notification to have a title and a message
+    private Notification setNotification(String title, String content) {
+        Intent notificationIntent = new Intent(this, UploadService.class);
+
+        int intentflags;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            intentflags = PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT;
+        } else {
+            intentflags = PendingIntent.FLAG_UPDATE_CURRENT;
+        }
+
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(this, 0, notificationIntent, intentflags);
+        Notification notification =
+                new Notification.Builder(this, "uploading-channel")
+                        .setContentTitle(title)
+                        .setContentText(content)
+                        .setSmallIcon(R.drawable.dna)
+                        .setContentIntent(pendingIntent)
+                        .build();
+
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.notify(5, notification);
+        return notification;
+    }
+
+    //enum to keep track of whether or not we are currently sending anything
+    public enum Status {
+        IDLE, SENDING, FAILED, SUCCESS
+    }
+
+    //according to android studio this can cause leaks, probably the source of the crashes
+    //reads the return codes of the batches and deletes files that were successfully uploaded
+    public class Sender extends AsyncTask<Batch, Integer, Void> {
+        @Override
+        protected Void doInBackground(Batch... batches) {
+            String code = batches[0].sendFiles();
+            if (code.equals("201")) {
+                batches[0].deleteFiles();
+                sendSuccessful(batches[0]);
+            } else {
+                sendFailure(code);
+            }
+            return null;
+        }
+    }
+
+    public class LocalBinder extends Binder {
+        public UploadService getService() {
+            return UploadService.this;
+        }
+    }
+}
+ */
